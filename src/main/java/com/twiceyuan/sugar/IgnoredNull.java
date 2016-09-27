e420e1a3a0593ca51664fbe1ddb514367358cd1d
twiceyuan <twiceyuan@gmail.com>
2016-09-27 18:03:06 +0800

    加入 IgnoreNull 工具, 用于忽略在引用过程中可能抛出 NPE 的情况, 只检查最后的结果

---
+++ src/main/java/com/twiceyuan/sugar/IgnoredNull.java
@@ -0,0 +1,60 @@
+package com.twiceyuan.sugar;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ * Created by twiceYuan on 27/09/2016.
+ * <p>
+ * 忽略空引用的对象, 类似 Optional, 传入获取值的函数来封装空引用检查
+ * 通过对值的 Provider 进行 Catch NPE 操作, 使调用过程不再关注空引用
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class IgnoredNull<T> {
+
+    private T mCacheValue;
+
+    /**
+     * 允许取值过程中所有引用可以为 null, 包括最终值
+     *
+     * @param provider 传入需要检测的获取值的函数
+     * @param <T>      该值的类型
+     * @return 该值的 IgnoredNull 对象
+     */
+    public static <T> IgnoredNull<T> of(@NotNull ValueProvider<T> provider) {
+        try {
+            return new IgnoredNull<>(provider.provide());
+        } catch (NullPointerException e) {
+            return new IgnoredNull<>(null);
+        }
+    }
+
+    private IgnoredNull(T t) {
+        mCacheValue = t;
+    }
+
+    public IgnoredNull<T> ifPresent(@NotNull ValueCallback<T> callback) {
+        if (mCacheValue != null) {
+            callback.call(mCacheValue);
+        }
+        return this;
+    }
+
+    public IgnoredNull<T> ifNull(@NotNull NullCallback callback) {
+        if (mCacheValue == null) {
+            callback.onNull();
+        }
+        return this;
+    }
+
+    public interface ValueProvider<T> {
+        T provide();
+    }
+
+    public interface ValueCallback<T> {
+        void call(T t);
+    }
+
+    public interface NullCallback {
+        void onNull();
+    }
+}
---
+++ src/test/java/com/twiceyuan/sugar/NullableTest.java
@@ -0,0 +1,61 @@
+package com.twiceyuan.sugar;
+
+import org.junit.Test;
+
+/**
+ * Created by twiceYuan on 27/09/2016.
+ * <p>
+ * 测试 IgnoredNull
+ */
+@SuppressWarnings("Convert2MethodRef")
+public class NullableTest {
+
+    @Test
+    public void test() {
+        String testString = "I'm a bug!";
+
+        IgnoredNull.of(() -> buildValue(testString).body.body.body).ifPresent((message) -> {
+            // 检测所有引用非空, 且值不为空
+            System.out.println(message);
+        }).ifNull(() -> {
+            // 该值为空或引用为空
+            System.out.println("该值为空或引用为空");
+        });
+
+        IgnoredNull.of(() -> buildBug(testString).body.body.body).ifPresent((message) -> {
+            // 检测所有引用非空, 且值不为空
+            System.out.println(message);
+        }).ifNull(() -> {
+            // 该值为空或引用为空
+            System.out.println("该值为空或引用为空");
+        });
+    }
+
+    private Bug<Bug<Bug<String>>> buildValue(String bugMessage) {
+        Bug<Bug<Bug<String>>> bug = new Bug<>();
+        Bug<Bug<String>> bugChild = new Bug<>();
+        Bug<String> bugMessageFoo = new Bug<>();
+
+        bug.body = bugChild;
+        bugChild.body = bugMessageFoo;
+        bugMessageFoo.body = bugMessage;
+
+        return bug;
+    }
+
+    private Bug<Bug<Bug<String>>> buildBug(String bugMessage) {
+        Bug<Bug<Bug<String>>> bug = new Bug<>();
+        Bug<Bug<String>> bugChild = new Bug<>();
+        Bug<String> bugMessageFoo = new Bug<>();
+
+        bug.body = bugChild;
+        bugChild.body = null; // a NPE
+        bugMessageFoo.body = bugMessage;
+
+        return bug;
+    }
+
+    private static class Bug<T> {
+        T body;
+    }
+}
