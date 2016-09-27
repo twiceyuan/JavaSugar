f2e0461a0c246443b7764b0123e57a42857dfc02
twiceyuan <twiceyuan@gmail.com>
2016-09-27 18:31:31 +0800

    修改测试用例

--- src/test/java/com/twiceyuan/sugar/TaskValveTest.java
+++ src/test/java/com/twiceyuan/sugar/TaskValveTest.java
@@ -2,9 +2,12 @@ package com.twiceyuan.sugar;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by twiceYuan on 4/29/16.
- *
+ * <p>
  * TaskValve unit test
  */
 public class TaskValveTest {
@@ -12,81 +15,44 @@ public class TaskValveTest {
     @Test
     public void test() {
         final TaskValve valve = TaskValve.create();
-        delayAndRun((long) (Math.random() * 1000), new Runnable() {
-            @Override
-            public void run() {
-                valve.addTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("步骤1, 等待 0 完成");
-                    }
-                });
-            }
+        delayAddTask(1200, valve, "步骤1");
+        delayAddTask(800, valve, "步骤2");
+        delayAddTask(500, valve, "步骤3");
+        delayAddTask(1500, valve, "步骤4");
+        delayAndRun(1000, () -> {
+            System.out.println("openValve at " + markTime());
+            valve.openValve();
         });
-        delayAndRun((long) (Math.random() * 1000), new Runnable() {
-            @Override
-            public void run() {
-                valve.addTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("步骤2, 等待 0 完成");
-                    }
-                });
+
+        sleep(3000);
+    }
+
+    private void delayAndRun(final long ms, final Runnable runnable) {
+        new Thread(() -> {
+            sleep(ms);
+            if (runnable != null) {
+                runnable.run();
             }
-        });
-        delayAndRun((long) (Math.random() * 1000), new Runnable() {
-            @Override
-            public void run() {
-                valve.addTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("步骤3, 等待 0 完成");
-                    }
-                });
-            }
-        });
-        delayAndRun((long) (Math.random() * 1000), new Runnable() {
-            @Override
-            public void run() {
-                valve.addTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("步骤4, 等待 0 完成");
-                    }
-                });
-            }
-        });
-        delayAndRun((long) (Math.random() * 1000), new Runnable() {
-            @Override
-            public void run() {
-                valve.addTask(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("步骤0");
-                    }
-                });
-            }
-        });
+        }).start();
+    }
+
+    private void sleep(long ms) {
         try {
-            Thread.sleep(2000);
+            Thread.sleep(ms);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private void delayAndRun(final long ms, final Runnable runnable) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(ms);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (runnable != null) {
-                    runnable.run();
-                }
-            }
+    private void delayAddTask(final long ms, TaskValve valve, String taskName) {
+        new Thread(() -> {
+            sleep(ms);
+            System.out.println(taskName + " create at: " + markTime());
+            valve.addTask(() -> System.out.println(taskName + " run at   : " + markTime()));
         }).start();
+    }
+
+    private String markTime() {
+        return new SimpleDateFormat("HH:mm:ss SSS").format(new Date());
     }
 }
